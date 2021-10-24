56f51d441653e57de186b09af1ab5c92e65a3adb
TeoDev1611 <malehurtadoreyes@hotmail.com>
2021-10-23 20:42:12 -0500

    feat: add the base for the login and blog page for fix the warn of router

---
+++ pages/login.vue
@@ -0,0 +1,26 @@
+<!--
+  NextChat - Messages social network
+  Copyright (C) 2021  NextChat Org & NextChat Contributors
+  
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-->
+
+<template>
+  <Header/>
+  <h1>Hello from login :p</h1>
+</template>
+
+<style lang="scss">
+@import '@/assets/styles/breakpoint', '@/assets/styles/variables';
+</style>
