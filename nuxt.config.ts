82c4da1a0ecb84ca809d1048abe4e2925b3fd4c5
TeoDev1611 <malehurtadoreyes@hotmail.com>
2021-10-23 20:33:45 -0500

    fix: remove the nuxtjs/apollo plugin for nuxtjs3 incompatibilitie

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,3 @@
 import { defineNuxtConfig } from 'nuxt3'
 
-export default defineNuxtConfig({
-  modules: ['@nuxtjs/apollo'],
-  apollo: {
-    clientConfigs: {
-      default: {
-        httpEndpoint: 'https://nextchat-api.herokuapp.com',
-      },
-    },
-  },
-})
+export default defineNuxtConfig({})
