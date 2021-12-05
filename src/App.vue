16ae54487ecdcbe67d70be52a3d9280c7709763d
Jheyson Saavedra <jheysonsaaved@gmail.com>
2021-12-05 09:49:36 -0500

    feat: add postcss, inter font as default, and alias config

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,22 @@
 <template>
   <router-view></router-view>
 </template>
+
+<style lang="scss">
+@import './assets/styles/normalize', './assets/styles/fonts';
+
+body > #app {
+  font-family: 'Inter';
+  text-rendering: optimizeLegibility;
+  height: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+
+  @supports (font-variation-settings: normal) {
+    & {
+      font-family: 'Inter var', sans-serif;
+    }
+  }
+}
+</style>
